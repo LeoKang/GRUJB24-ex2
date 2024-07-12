1a600bc1473b3801f79c43d37e8a67f74b6c7f5f
LeoKang <xeungjin@nate.com>
2024-07-12 12:55:23 +0900

    ~ 60 page

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,6 +3,10 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.ex2.entity.Memo;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class MemoRepositoryTests {
@@ -13,5 +17,25 @@ public class MemoRepositoryTests {
     @Test
     public void testClass() {
         System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testSelect() {
+        Long mno = 400L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+        System.out.println("===================================");
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
     }
 }
